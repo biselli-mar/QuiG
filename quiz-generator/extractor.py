1e4b864f3e96b9e19d335008a5a5341cd04b176b
Nadine Görzen <namied@pm.me>
2024-07-28 22:24:56 +0200

    add latex support

--- quiz-generator/extractor.py
+++ quiz-generator/extractor.py
@@ -4,20 +4,39 @@ import pymupdf4llm
 from pylatexenc.latex2text import LatexNodes2Text
 
 
-# Text extraction functions
-def extract_text_from_pdf(pdf_path):
-    file = pymupdf.open(stream=pdf_path, filetype="pdf")  # sort=True
+def extract_text_from_pdf(pdf_stream):
+    file = pymupdf.open(stream=pdf_stream, filetype="pdf")  # sort=True
     md_text = pymupdf4llm.to_markdown(file, write_images=False, page_chunks=False)
-
-    # remove multiple newlines and trailing whitespaces
-    md_text = re.sub("\n\n+", "\n", md_text).strip()
+    md_text = clean_text(md_text)
 
     file.close()
     return md_text
 
 
-def extract_text_from_latex(latex_path):
-    with open(latex_path, 'r') as file:
-        latex_content = file.read()
+def extract_text_from_latex(latex_stream):
+    latex_content = latex_stream.decode("utf-8")
     text = LatexNodes2Text().latex_to_text(latex_content)
+    text = clean_text(text)
+
+    return text
+
+
+def clean_text(text):
+    # Replace multiple newlines with a single newline
+    text = re.sub(r'\n\s*\n+', '\n', text)
+
+    # Replace multiple spaces with a single space
+    text = re.sub(r' +', ' ', text)
+
+    # Replace multiple tabs with a single tab
+    text = re.sub(r'\t+', '\t', text)
+
+    # Ensure lines containing only tabs or spaces are reduced to a single newline
+    lines = text.split('\n')
+    lines = [line if not re.match(r'^\s*$', line) else '' for line in lines]
+    text = '\n'.join(lines)
+
+    # Strip leading and trailing whitespace from the entire text
+    text = text.strip()
+
     return text
