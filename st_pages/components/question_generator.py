7958392ef46a3a10a58e620b49eec3d3b6b4ccd4
biselli-mar <ma741bis@htwg-konstanz.de>
2024-12-20 00:44:42 +0100

    Move logic for summaries into separate component

--- st_pages/components/question_generator.py
+++ st_pages/components/question_generator.py
@@ -1,49 +1,15 @@
 import logging
 import streamlit as st
-import datetime
-import json
-from const import RECENT_SUMMARIES_PATH
 from langchain_core.exceptions import OutputParserException
 from openai import APIConnectionError, AuthenticationError
 
 from generation.question_generation_chain import generate_questions
 from generation.summarization import summarize_docs, split_text
+from st_pages.components.recent_summaries import append_summary_dialog
 from quiz.export import convert_to_gift
 from quiz.question_list import list_questions
 
 selected_questions = []
-
-@st.dialog("Save Summary")
-def append_summary_dialog():
-    f_recent_summaries = open(RECENT_SUMMARIES_PATH, "r")
-    recent_summaries = f_recent_summaries.read()
-    if recent_summaries == "":
-        recent_summaries = "[]"
-    f_recent_summaries.close()
-    time_now = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
-    summary = st.session_state.last_summary
-    with st.form(key="append_summary"):
-        title = st.text_input("Title", value="Recent Summary", placeholder="Enter a title for the summary.")
-        summary_input = st.text_area("Text", summary, height=200)
-        col1, col2 = st.columns([1, 1])
-        with col1:
-            save = st.form_submit_button("Save", use_container_width=True)
-        with col2:
-            cancel = st.form_submit_button("Cancel", use_container_width=True)
-        if save:
-            summaries_json = json.loads(recent_summaries)
-            summaries_json.append({"time":f"{time_now}","title":f"{title}","summary":summary_input})
-            append_summary(summaries_json)
-            st.rerun()
-            st.write("Summary saved.")
-        if cancel:
-            st.rerun()
-
-def append_summary(recent_summaries):
-    f_recent_summaries = open(RECENT_SUMMARIES_PATH, "w")
-    f_recent_summaries.write(json.dumps(recent_summaries, default=lambda x: x.__dict__))
-    f_recent_summaries.close()
-
 
 def reset_state():
     st.session_state.generated = False
@@ -51,10 +17,10 @@ def reset_state():
     logging.info("State reset.")
 
 
-def question_generator(text_input):
+def question_generator(text):
     st.divider()
+    text_input = st.text_area("Text", text, height=300)
     st.header("Questions")
-
     with st.form("generate_questions"):
         num_questions = st.select_slider("Number of questions",
                                          options=range(1, 11), value=5, help="Select the number of questions "
